3578b7f1c1e7f3788c310554450d5e7695a45c22
Aman Raghav <amanraghavkumar@gmail.com>
2025-04-19 19:07:59 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // output: 'export',
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+//   basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+//   assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+// };
+
+// module.exports = nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
